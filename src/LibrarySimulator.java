40b8e9375a5c6ce15acc1b362100382eebc7cabb
hustleksa <Saifalmojel663@gmail.com>
2025-10-16 17:18:01 +0300

    Update LibrarySimulator.java

--- src/LibrarySimulator.java
+++ src/LibrarySimulator.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;
+//This is a project about LibrarySimulator
 public class LibrarySimulator {
+    //Abdulrahman variables here
+
+
+    //Yazzan variables here
+
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
 boolean Running = true;
+//This is loop for the main menu
 while(Running){
     System.out.println("================================");
     System.out.println("Welcome to the library Simulator");
@@ -12,22 +20,30 @@ while(Running){
     System.out.println("2-Login as Administrator ");
     System.out.println("3-Exit the program");
     int choice =input.nextInt();
+    //This is the will take you to the user page
     if(choice==1) {
         System.out.println("1 ");
 
-    } else if(choice==2){
+    }
+    //This is the will take you to the Admin page
+
+    else if(choice==2){
         System.out.println("2 ");
 
     }
+    //this will close the loop
                 else if(choice==3) {
         Running = false;
         System.out.println("Thanks for using our Library Simulator. Goodbye! ");
     }
+                //if user enter an invalid choice
 else{
                 System.out.println("Invalid choice please try again");
 
             }
 
     }
+//scanner closed
+input.close();
 }
     }
