a272ca30a907d37e1c4f04c3d910b60450b0e984
d7d7-d <aawwppiiii75@gmail.com>
2025-10-17 22:26:10 +0300

    Implement user accounts and borrowing functionality
    Added variables and functionality for three user accounts in the LibrarySimulator.

--- src/LibrarySimulator.java
+++ src/LibrarySimulator.java
@@ -1,13 +1,32 @@
 import java.util.Scanner;
 //This is a project about LibrarySimulator
 public class LibrarySimulator {
-    //Abdulrahman variables here
-
-
-    //Yazzan variables here
-
 
     public static void main(String[] args) {
+
+        //Abdulrahman variables here
+        // The info first Account
+        int ID_num1 = 1011223344;
+        String NameAccount1 = "Omar";
+        int Books_Currently_Borrowed1 = 3;
+
+
+        // The info for Account number 2
+        int ID_num2 = 1219235384;
+        String NameAccount2 = "Laila";
+        int Books_Currently_Borrowed2 = 1;
+
+        // The info for Account number 3
+        int ID_num3 = 1352226662;
+        String NameAccount3 = "Adam";
+        int Books_Currently_Borrowed3 = 4;
+
+
+
+
+
+        //Yazzan variables here
+
         Scanner input = new Scanner(System.in);
 
 boolean Running = true;
@@ -22,8 +41,251 @@ while(Running){
     int choice =input.nextInt();
     //This is the will take you to the user page
     if(choice==1) {
-        System.out.println("1 ");
+        //Abdulrahman variables here
+        boolean Running_User_Page = true;
+        int Books_borrowed_count = 0;
+        int Number_of_books_returned = 0;
+        double system_charges_per_borrowed_book = 0.0;
 
+        // this will make a menu for the user for all the accounts then he will select one of them
+        System.out.println("Select Your Account: ");
+        System.out.println("_______________________________________________");
+        System.out.printf("ID\t\tName\t\tBooksCurrently Borrowed : \n");
+        System.out.println("1-\t" + ID_num1 + "\t" + NameAccount1 + "\t" + Books_Currently_Borrowed1);
+        System.out.println("2- \t" + ID_num2 + "\t" + NameAccount2 + "\t" + Books_Currently_Borrowed2);
+        System.out.println("3-\t" + ID_num3 + "\t" + NameAccount3 + "\t" + Books_Currently_Borrowed3);
+        System.out.println("_______________________________________________");
+
+        int userChoice = input.nextInt();
+
+        // if the user select the first account will go here
+        if (userChoice == 1) {
+
+            //to run the user page
+            while (Running_User_Page) {
+
+                //The user will be shown the options that can be done
+                System.out.println("Choose form the following: ");
+                System.out.println("1- View Borrowed Books Count ");
+                System.out.println("2- Borrow Book ");
+                System.out.println("3- Return Book");
+                System.out.println("4- View Session Summary ");
+                System.out.println("5- Exit to Main Menu ");
+
+                // The variable is choice for user page
+                int choice_U_P = input.nextInt();
+
+
+                switch (choice_U_P) {
+                    //this is the first option
+                    case 1:
+                        System.out.println("The number of books you have is: " + Books_Currently_Borrowed1);
+                        break;
+
+                    //this is the second option
+                    case 2:
+                        if (Books_Currently_Borrowed1 < 5) {
+                            System.out.println("The system charges a 0.50 credit fee per borrowed book.");
+                            System.out.println("Choose Frome The Following books to borrows: ");
+                            System.out.println("1- Rich Dad Poor Dad");
+                            System.out.println("2- The 7 Habits of Highly Effective People");
+                            System.out.println("3- The Power of Habit");
+                            System.out.println("4- The Psychology of Money");
+                            System.out.println("5- Start with Why");
+
+                            int choice_Books_borrowed_count = input.nextInt();
+
+                            ++Books_borrowed_count;
+                            ++Books_Currently_Borrowed1;
+
+                            system_charges_per_borrowed_book += 0.50;
+
+                        } else {
+                            System.out.println(" cannot borrow more than 5 books ");
+                        }
+
+                        break;
+
+                    //this third option
+                    case 3:
+                        if (Books_Currently_Borrowed1 >= 1) {
+                            System.out.println("There is no fee for Returning books");
+
+
+                            --Books_Currently_Borrowed1;
+
+                            ++Number_of_books_returned;
+                        } else
+                            System.out.println("You have no book to Return");
+                        break;
+                    //This is the fourth option
+                    case 4:
+                        System.out.println("This is the Session Summary: ");
+
+                        System.out.println("Number of books borrowed: " + Books_borrowed_count);
+                        System.out.println("Number of books returned: " + Number_of_books_returned);
+                        System.out.printf("Total fees incurred during the session: %.2f \n" , system_charges_per_borrowed_book);
+                        break;
+
+                    // This is the option  number 5
+                    case 5:
+                        Running_User_Page = false;
+                        break;
+
+                    default:
+                        System.out.println("Somthing went wrong...! Pick number form the lits: ");
+                }
+
+            }
+
+
+        }
+        // this is for the second user
+        else if (userChoice == 2) {
+            while (Running_User_Page) {
+
+                System.out.println("Choose form the following: ");
+                System.out.println("1- View Borrowed Books Count ");
+                System.out.println("2- Borrow Book ");
+                System.out.println("3- Return Book");
+                System.out.println("4- View Session Summary ");
+                System.out.println("5- Exit to Main Menu ");
+
+                // The variable is choice for user page
+                int choice_U_P = input.nextInt();
+
+
+                switch (choice_U_P) {
+                    case 1:
+                        System.out.println("The number of books you have is: " + Books_Currently_Borrowed2);
+                        break;
+                    case 2:
+                        if (Books_Currently_Borrowed2 < 5) {
+                            System.out.println("The system charges a 0.50 credit fee per borrowed book.");
+                            System.out.println("Choose Frome The Following books to borrows: ");
+                            System.out.println("1- Rich Dad Poor Dad");
+                            System.out.println("2- The 7 Habits of Highly Effective People");
+                            System.out.println("3- The Power of Habit");
+                            System.out.println("4- The Psychology of Money");
+                            System.out.println("5- Start with Why");
+
+                            int choice_Books_borrowed_count = input.nextInt();
+
+                            ++Books_borrowed_count;
+                            ++Books_Currently_Borrowed2;
+
+                            system_charges_per_borrowed_book += 0.50;
+
+                        } else {
+                            System.out.println(" cannot borrow more than 5 books ");
+                        }
+
+                        break;
+
+                    case 3:
+                        if (Books_Currently_Borrowed2 >= 1) {
+                            System.out.println("There is no fee for Returning books");
+
+                            --Books_Currently_Borrowed2;
+
+                            ++Number_of_books_returned;
+                        } else
+                            System.out.println("You have no book to Return");
+                        break;
+
+                    case 4:
+                        System.out.println("This is the Session Summary: ");
+
+                        System.out.println("Number of books borrowed: " + Books_borrowed_count);
+                        System.out.println("Number of books returned: " + Number_of_books_returned);
+                        System.out.printf("Total fees incurred during the session: %.2f \n" , system_charges_per_borrowed_book);
+                        break;
+
+                    case 5:
+                        Running_User_Page = false;
+                        break;
+
+                    default:
+                        System.out.println("Somthing went wrong...! Pick number form the lits: ");
+                }
+
+            }
+        }
+        // this is for the user number 3
+        else if (userChoice == 3) {
+            while (Running_User_Page) {
+
+                System.out.println("Choose form the following: ");
+                System.out.println("1- View Borrowed Books Count ");
+                System.out.println("2- Borrow Book ");
+                System.out.println("3- Return Book");
+                System.out.println("4- View Session Summary ");
+                System.out.println("5- Exit to Main Menu ");
+
+                // The variable is choice for user page
+                int choice_U_P = input.nextInt();
+
+
+                switch (choice_U_P) {
+                    case 1:
+                        System.out.println("The number of books you have is: " + Books_Currently_Borrowed3);
+                        break;
+                    case 2:
+                        if (Books_Currently_Borrowed3 < 5) {
+                            System.out.println("The system charges a 0.50 credit fee per borrowed book.");
+                            System.out.println("Choose Frome The Following books to borrows: ");
+                            System.out.println("1- Rich Dad Poor Dad");
+                            System.out.println("2- The 7 Habits of Highly Effective People");
+                            System.out.println("3- The Power of Habit");
+                            System.out.println("4- The Psychology of Money");
+                            System.out.println("5- Start with Why");
+
+                            int choice_Books_borrowed_count = input.nextInt();
+
+                            ++Books_borrowed_count;
+                            ++Books_Currently_Borrowed3;
+
+                            system_charges_per_borrowed_book += 0.50;
+
+                        } else {
+                            System.out.println(" cannot borrow more than 5 books ");
+                        }
+
+                        break;
+
+                    case 3:
+                        if (Books_Currently_Borrowed3 >= 1) {
+                            System.out.println("There is no fee for Returning books");
+
+                            --Books_Currently_Borrowed3;
+
+                            ++Number_of_books_returned;
+                        } else
+                            System.out.println("You have no book to Return");
+                        break;
+
+                    case 4:
+                        System.out.println("This is the Session Summary: ");
+
+                        System.out.println("Number of books borrowed: " + Books_borrowed_count);
+                        System.out.println("Number of books returned: " + Number_of_books_returned);
+                        System.out.printf("Total fees incurred during the session: %.2f \n" , system_charges_per_borrowed_book);
+                        break;
+
+                    case 5:
+                        Running_User_Page = false;
+                        break;
+
+                    default:
+                        System.out.println("Somthing went wrong...! Pick number form the lits: ");
+                }
+
+            }
+
+        }
+        else {
+            System.out.println("Somthing went wrong...! Pick number form the lits: ");
+        }
     }
     //This is the will take you to the Admin page
 
